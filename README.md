5d74cf64ba22554269a96c86b80faf523d71ae49
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2017-02-09 23:34:20 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# INFO3180 Lab 4 Starter Code
+This is the starter code for Lab 4 which is requires you to build a Flask app that does simple file uploads and has basic authentication.
---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+# Config Values
+USERNAME = 'admin'
+PASSWORD = 'password123'
+
+# SECRET_KEY is needed for session security, the flash() method in this case stores the message in a session
+SECRET_KEY = 'Sup3r$3cretkey'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+from app import views
