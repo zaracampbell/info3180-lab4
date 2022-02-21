6037bcb4ac4e79713cc3502aaf1a1476dc09d3b6
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2022-02-20 23:25:13 -0500

    Add python-dotenv and simpliy config.py

--- app/config.py
+++ app/config.py
@@ -1,18 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config(object):
     """Base Config Object"""
     DEBUG = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
-    ADMIN_USERNAME = os.environ.get('ADMIN_USERNAME') or 'admin'
-    ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD') or 'Password123'
-
-
-class DevelopmentConfig(Config):
-    """Development Config that extends the Base Config Object"""
-    DEVELOPMENT = True
-    DEBUG = True
-
-class ProductionConfig(Config):
-    """Production Config that extends the Base Config Object"""
-    DEBUG = False
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
+    ADMIN_USERNAME = os.environ.get('ADMIN_USERNAME', 'admin')
+    ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD', 'Password123')
