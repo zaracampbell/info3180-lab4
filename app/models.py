55fc015ccf3662eca6cac761b73a13e866415dbe
zaracampbell <campbellzara13@gmail.com>
2025-04-12 03:05:51 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -12,14 +12,7 @@ class UserProfile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
-    username = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(256))
-
-    def __init__(self, first_name,last_name, username, password):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.username = username
-        self.password = generate_password_hash(password) 
+    username = db.Column(db.String(80), unique=True) 
 
     def is_authenticated(self):
         return True
