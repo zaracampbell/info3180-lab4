679164c5acb1a2be871ff6d7b3bd43ca39f360b3
zaracampbell <campbellzara13@gmail.com>
2025-04-12 03:04:31 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash
 
 
 class UserProfile(db.Model):
@@ -12,6 +13,13 @@ class UserProfile(db.Model):
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
     username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(256))
+
+    def __init__(self, first_name,last_name, username, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = generate_password_hash(password) 
 
     def is_authenticated(self):
         return True
